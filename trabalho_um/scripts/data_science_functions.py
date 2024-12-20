0a31b9399afc8f1863ff6f79f8a3e3124d2b3648
JoaoVitorOliveiraa <jvoliveira30@gmail.com>
2024-12-20 10:36:23 -0300

    Criação de um arquivo que contém funções úteis para aplicações de Ciência de Dados.

---
+++ trabalho_um/scripts/data_science_functions.py
@@ -0,0 +1,58 @@
+#==============================================================================
+# Arquivo que contém funções úteis para aplicações de Ciência de Dados
+#==============================================================================
+
+def show_data_head(lines_number, data, features=False):
+    "Função que mostra as primeiras 'lines_number' linhas do DataFrame 'data'."
+
+    if features:
+        for feature in features:
+            print(f'\n{data[feature].head(n=lines_number)}\n')
+
+    else:
+        print(f'\n{data.head(n=lines_number)}\n')
+
+
+def show_data_tail(lines_number, data, features=False):
+    "Função que mostra as últimas 'lines_number' linhas do DataFrame 'data'."
+
+    if features:
+        for feature in features:
+            print(f'\n{data[feature].tail(n=lines_number)}\n')
+
+    else:
+        print(f'\n{data.tail(n=lines_number)}\n')
+
+
+def show_data_info(data, features=False):
+    "Função que mostra informações sobre o DataFrame 'data', incluindo o tipo de índice e colunas, valores não nulos e uso de memória."
+
+    if features:
+        for feature in features:
+            print(f'\n{data[feature].info()}\n')
+
+    else:
+        print(f'\n{data.info()}\n')
+
+
+def show_data_value_counts(data, features=False):
+    "Função que mostra a frequência das classes de cada feature no DataFrame 'data'."
+
+    if features:
+        for feature in features:
+            print(f'\n{data[feature].value_counts()}\n')
+
+    else:
+        for feature in data.columns:
+            print(f'\n{data[feature].value_counts()}\n')
+
+
+def show_data_description(data, features=False):
+    "Função que mostra uma descrição estatística da DataFrame 'data', incluindo os três quartis, média e desvio padrão."
+
+    if features:
+        for feature in features:
+            print(f'\n{data[feature].describe()}\n')
+
+    else:
+        print(f'\n{data.describe()}\n')
