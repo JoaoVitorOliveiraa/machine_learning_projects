0a576f72285ae99b78ae03a7895d9ea203614a56
JoaoVitorOliveiraa <jvoliveira30@gmail.com>
2024-12-20 10:51:25 -0300

    Renomeação de variáveis.

--- trabalho_um/scripts/data_science_functions.py
+++ trabalho_um/scripts/data_science_functions.py
@@ -2,57 +2,57 @@
 # Arquivo que contém funções úteis para aplicações de Ciência de Dados
 #==============================================================================
 
-def show_data_head(lines_number, data, features=False):
+def show_data_head(lines_number, data, columns=False):
     "Função que exibe as primeiras 'lines_number' linhas do DataFrame 'data'."
 
-    if features:
-        for feature in features:
-            print(f'\n{data[feature].head(n=lines_number)}\n')
+    if columns:
+        for column in columns:
+            print(f'\n{data[column].head(n=lines_number)}\n')
 
     else:
         print(f'\n{data.head(n=lines_number)}\n')
 
 
-def show_data_tail(lines_number, data, features=False):
+def show_data_tail(lines_number, data, columns=False):
     "Função que exibe as últimas 'lines_number' linhas do DataFrame 'data'."
 
-    if features:
-        for feature in features:
-            print(f'\n{data[feature].tail(n=lines_number)}\n')
+    if columns:
+        for column in columns:
+            print(f'\n{data[column].tail(n=lines_number)}\n')
 
     else:
         print(f'\n{data.tail(n=lines_number)}\n')
 
 
-def show_data_info(data, features=False):
+def show_data_info(data, columns=False):
     "Função que exibe informações sobre o DataFrame 'data', incluindo o tipo de índice e colunas, valores não nulos e uso de memória."
 
-    if features:
-        for feature in features:
-            print(f'\n{data[feature].info()}\n')
+    if columns:
+        for column in columns:
+            print(f'\n{data[column].info()}\n')
 
     else:
         print(f'\n{data.info()}\n')
 
 
-def show_data_value_counts(data, features=False):
-    "Função que exibe a frequência das classes de cada feature no DataFrame 'data'."
+def show_data_value_counts(data, columns=False):
+    "Função que exibe a frequência das classes de cada coluna no DataFrame 'data'."
 
-    if features:
-        for feature in features:
-            print(f'\n{data[feature].value_counts()}\n')
+    if columns:
+        for column in columns:
+            print(f'\n{data[column].value_counts()}\n')
 
     else:
-        for feature in data.columns:
-            print(f'\n{data[feature].value_counts()}\n')
+        for column in data.columns:
+            print(f'\n{data[column].value_counts()}\n')
 
 
-def show_data_description(data, features=False):
+def show_data_description(data, columns=False):
     "Função que exibe uma descrição estatística do DataFrame 'data', incluindo os três quartis, média e desvio padrão."
 
-    if features:
-        for feature in features:
-            print(f'\n{data[feature].describe()}\n')
+    if columns:
+        for column in columns:
+            print(f'\n{data[column].describe()}\n')
 
     else:
         print(f'\n{data.describe()}\n')
