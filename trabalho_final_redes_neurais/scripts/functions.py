8a547a4e8daf516d3fd14e5433dd6cb75485235e
JoaoVitorOliveiraa <jvoliveira30@gmail.com>
2025-06-29 14:55:34 -0300

    Atualização da função calcular_metricas_agrupadas.

--- trabalho_final_redes_neurais/scripts/functions.py
+++ trabalho_final_redes_neurais/scripts/functions.py
@@ -36,29 +36,62 @@ def calcular_metricas_agrupadas(linha, coluna_alvo, status_df):
 
     valores_na_linha = linha[coluna_alvo]
 
-    # Caso 1: Valor único
+    # Caso 1: Valor único.
     if len(valores_na_linha) == 1:
         valor = valores_na_linha[0]
-        stats = status_df[status_df[coluna_alvo] == valor].iloc[0]
-        return pd.Series({
-            f'{coluna_alvo}_count': stats[f'qtd_filmes_{coluna_alvo.lower()}'],
-            f'{coluna_alvo}_mean_rating': stats[f'media_notas_{coluna_alvo.lower()}']
-        })
+        status = status_df[status_df[coluna_alvo] == valor].iloc[0]
+        
+        # Verifica se não está vazio.
+        if not status.empty:
+            return pd.Series({
+                f'{coluna_alvo}_count': status[f'qtd_filmes_{coluna_alvo.lower()}'],
+                f'{coluna_alvo}_mean_rating': status[f'media_ponderada_{coluna_alvo.lower()}'],
+                f'{coluna_alvo}_mean_per_qtd': status[f'media_ponderada_{coluna_alvo.lower()}'] / status[f'qtd_filmes_{coluna_alvo.lower()}']
+            })
+        
+        # Se estiver, retorna valores zerados.
+        else:
+            return pd.Series({f'{coluna_alvo}_count': 0.0, f'{coluna_alvo}_mean_rating': 0.0, f'{coluna_alvo}_mean_per_qtd': 0.0})
 
-    # Caso 2: Valores únicos (todos com count == 1)
-    elif all(status_df[status_df[coluna_alvo] == v][f'qtd_filmes_{coluna_alvo.lower()}'].iloc[0] == 1
-             for v in valores_na_linha):
-        return pd.Series({
-            f'{coluna_alvo}_count': 1.0,
-            f'{coluna_alvo}_mean_rating': linha['Average_rating']
-        })
+    # Caso 2: Valores únicos (todos com count == 1).
+    elif all(status_df[status_df[coluna_alvo] == valor][f'qtd_filmes_{coluna_alvo.lower()}'].iloc[0] == 1
+             for valor in valores_na_linha):
+        
+        # Neste caso, podemos tomar o primeiro valor. Pois as métricas de todos são iguais.
+        valor = valores_na_linha[0]
+        status = status_df[status_df[coluna_alvo] == valor].iloc[0]
+        
+        # Verifica se não está vazio.
+        if not status.empty:
+            return pd.Series({
+                f'{coluna_alvo}_count': 1.0,
+                f'{coluna_alvo}_mean_rating': status[f'media_ponderada_{coluna_alvo.lower()}'],
+                f'{coluna_alvo}_mean_per_qtd': status[f'media_ponderada_{coluna_alvo.lower()}'] / 1.0
+            })
 
-    # Caso 3: Valores com histórico (calcula médias)
+        # Se estiver, retorna valores zerados.
+        else:
+            return pd.Series({f'{coluna_alvo}_count': 0.0, f'{coluna_alvo}_mean_rating': 0.0, f'{coluna_alvo}_mean_per_qtd': 0.0})
+
+    # Caso 3: Quantidade de filmes diferentes entre os diretores.
     else:
-        stats = status_df[status_df[coluna_alvo].isin(valores_na_linha)]
-        return pd.Series({
-            f'{coluna_alvo}_count': stats[f'qtd_filmes_{coluna_alvo.lower()}'].mean(),
-            f'{coluna_alvo}_mean_rating': stats[f'media_notas_{coluna_alvo.lower()}'].mean()
-        })
 
+        # Seleciona apenas as linhas dos valores da linha.        
+        status = status_df[status_df[coluna_alvo].isin(valores_na_linha)]
+    
+        # Verifica se não está vazio.
+        if not status.empty:
+            quantidade_media_filmes = status[f'qtd_filmes_{coluna_alvo.lower()}'].mean()
+            media_ponderada_media = status[f'media_ponderada_{coluna_alvo.lower()}'].mean()
+            
+            return pd.Series({
+                f'{coluna_alvo}_count': quantidade_media_filmes,
+                f'{coluna_alvo}_mean_rating': media_ponderada_media,
+                f'{coluna_alvo}_mean_per_qtd': media_ponderada_media / quantidade_media_filmes
+            })
+         
+        # Se estiver, retorna valores zerados.
+        else: 
+            return pd.Series({f'{coluna_alvo}_count': 0.0, f'{coluna_alvo}_mean_rating': 0.0, f'{coluna_alvo}_mean_per_qtd': 0.0})
+ 
 # ------------------------------------------------------------------------------
